d5cfde17a18096bfcea0d32d6b41aa267c4e99bc
Edward Atkinson <edwardatkinson@Edwards-iMac.local>
2020-09-09 10:07:36 -0700

    Updates

---
+++ precache-manifest.fff22f85d9d3f916ffd7553bda6984bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb676f09b15cc7c0098d1544faf97623",
+    "url": "/facialrecognition/index.html"
+  },
+  {
+    "revision": "835f12a02e912b6fa06d",
+    "url": "/facialrecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5c7c49e8091f31203181",
+    "url": "/facialrecognition/static/css/main.34ee6bcf.chunk.css"
+  },
+  {
+    "revision": "835f12a02e912b6fa06d",
+    "url": "/facialrecognition/static/js/2.5618281b.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facialrecognition/static/js/2.5618281b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c7c49e8091f31203181",
+    "url": "/facialrecognition/static/js/main.dea084e3.chunk.js"
+  },
+  {
+    "revision": "de07a3eef01ef38fefaf",
+    "url": "/facialrecognition/static/js/runtime-main.2dedb02f.js"
+  }
+]);
